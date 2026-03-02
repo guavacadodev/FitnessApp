303500049646eef65eb51a28a72326c138eb6a48
guavacadodev <jakewoodall001@gmail.com>
2026-03-01 20:23:45 -0800

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Jake Woodall on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Jake Woodall on 3/1/26.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
